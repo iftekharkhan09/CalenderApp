f635d4f6d99e14fe9ecc233080c0d1fa1070477e
Prakash Upadhyay <prakashupadhyay770@gmail.com>
2019-07-31 00:01:26 +0530

    Added dragged element

--- src/components/display-by.js
+++ src/components/display-by.js
@@ -1,67 +1,80 @@
 import React, {Component} from 'react';
 import './display-by.css';
 import GridComponent from "./grid-component";
+import {Rnd} from "react-rnd";
 
 class DisplayBy extends Component{
     render() {
 
         return (
-            <div className="grid">
-                <div className="columnName">Coke 330 ml</div>
-                <div className="columnName1"></div>
-               <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <div>Cole lite 330 ml</div>
+            <div>
+                <Rnd
+                    default={{
+                        x: 0,
+                        y: 0,
+                        width: 320,
+                        height: 200,
+                    }}
+                >
+                    <div className='draggable-item'> Drag me</div>
+                </Rnd>
+                <div className="grid">
+                    <div className="columnName">Coke 330 ml</div>
                     <div className="columnName1"></div>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <div>Fanta 330 ml</div>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <div>Cole lite 330 ml</div>
                     <div className="columnName1"></div>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <div>Limca 330 ml</div>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <div>Fanta 330 ml</div>
                     <div className="columnName1"></div>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <div>Orange 330 ml<br></br></div>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <div>Limca 330 ml</div>
                     <div className="columnName1"></div>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <div>Sprite 330 ml</div>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <div>Orange 330 ml<br></br></div>
                     <div className="columnName1"></div>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
-                <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <div>Sprite 330 ml</div>
+                    <div className="columnName1"></div>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
+                    <GridComponent/>
                 </div>
+            </div>
         );
     }
 }
